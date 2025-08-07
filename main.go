4a15fb644046e63361ae6f0aa81e1258edcaa3ee
Reshav Gupta <111631321+Mca-reshav@users.noreply.github.com>
2025-08-07 15:41:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,3 @@
-// main.go
-
-/*
-Def :
-    Go is a statically typed, compiled programming language designed at Google.
-    It was designed at Google in 2007 by Robert Griesemer, Rob Pike, and Ken Thompson.
-
-Features :
-    - Simplicity and readability
-    - Fast compilation
-    - Built-in concurrency support
-    - Strong support for modularity, testing, and tooling
-*/
-
 package main
 
 import "fmt" // standard package lib : format
